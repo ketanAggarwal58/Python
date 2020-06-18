bb96b8e88b73f9a64c1508ecb1af1575c71abcf7
KetanAggarwal58 <ketanaggarwal58@gmail.com>
2020-06-18 09:48:20 +0530

    now if_else displays the no. in result

--- if_else.py
+++ if_else.py
@@ -6,10 +6,10 @@ print("building an even odd function in python using if else condition");
 
 a = int(input("please enter an integer: "));
 
-if (a%2 == 0):
+if a%2 == 0:
 
-    print("this is an even no.");
+    print(str(a)+" is an even no.");
 
 else:
     
-    print("this is an odd no.");
+    print(str(a)+" is an odd no.");
