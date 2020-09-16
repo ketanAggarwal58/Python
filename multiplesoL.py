e4eeb25fe832426781fb507303bc7be7e6486494
KetanAggarwal58 <ketanaggarwal58@gmail.com>
2020-09-16 12:21:59 +0530

    intial commit

---
+++ multiplesoL.py
@@ -0,0 +1,9 @@
+# Write a Python program to multiplies all the items in a list.
+
+a = [1,2,3,4,5,6,7,8,9,10]
+multiple = 1
+
+for x in a:
+    multiple = multiple*x
+
+print(multiple)
---
+++ sumol.py
@@ -0,0 +1,9 @@
+# Write a Python program to sum all the items in a list.
+
+a = [1,2,3,4,5,6,7,8,9,10]
+sum = 0
+
+for x in a:
+    sum = sum + x
+
+print(sum)
